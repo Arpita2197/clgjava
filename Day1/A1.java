1957ea2c0c6c81e07122a467d629956685a53c53
unknown <@arpitasahu2197@gmail.com>
2025-04-08 10:18:00 +0530

    java

---
+++ Day1/A1.java
@@ -0,0 +1,21 @@
+class A1 {
+
+   Q1  //  public static void main (String [] args) {
+
+    //      System.out.println("Hello, World") ;
+    //  }
+   
+
+  //Q2
+
+   
+
+    //Q3
+
+     int a = 40;
+     float b=a;
+
+        public static void 
+      System.out.println(b);
+      
+}
